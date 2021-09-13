71827236aceb9ba08fb2bc427fd7fc79e9ff40f5
Sergio Sanchez <ssanchez@alluma.org>
2021-09-13 10:37:53 -0700

    From docs

--- validate_snowflake.py
+++ validate_snowflake.py
@@ -1,15 +1,15 @@
-import os 
+import os
 from sqlalchemy import create_engine
 
 
-SF_USER = os.environ['SF_USER']
-SF_PASS = os.environ['SF_PASS']
+SF_USER = os.environ["SF_USER"]
+SF_PASS = os.environ["SF_PASS"]
 SF_ACCT = os.environ["SF_ACCT"]
 
-engine = create_engine(f'snowflake://{SF_USER}:{SF_PASS}@{SF_ACCT}/')
+engine = create_engine(f"snowflake://{SF_USER}:{SF_PASS}@{SF_ACCT}/")
 try:
     connection = engine.connect()
-    results = connection.execute('select current_version()').fetchone()
+    results = connection.execute("select current_version()").fetchone()
     print(results[0])
 finally:
     connection.close()
